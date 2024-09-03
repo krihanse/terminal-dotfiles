4c86e6715bda2fe32d7700f5414a8e382dd66f60
Kristian Hansen <krihanse@gmail.com>
2024-09-03 23:21:01 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ tar xz --strip 1 -C ~/.local/Homebrew
 mkdir -p ~/.local/bin &&
 ln -s ~/.local/Homebrew/bin/brew ~/.local/bin
 
+# Install zinit plugin manager
+# https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+
 # update brew
 brew update
 
@@ -18,10 +23,6 @@ brew install font-fira-code-nerd-font
 
 # https://starship.rs/guide/#%F0%9F%9A%80-installation
 brew install starship
-
-# https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
-bash -c "$(curl --fail --show-error --silent \
-    --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 # https://github.com/nvbn/thefuck?tab=readme-ov-file#installation
 brew install thefuck
@@ -56,10 +57,22 @@ brew install komposer
 # install kubeconform
 brew install kubeconform
 
+# install neovim
+brew install neovim
+
+# install helm
+brew install helm
+
+# install stow
+brew install stow
+
+# install kubie
+brew install kubie
+
 # https://www.jetify.com/devbox/docs/installing_devbox/
 # curl -fsSL https://get.jetify.com/devbox | bash
 
 # install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 #sudo rm -rf /opt/nvim
-tar -C ~/.local/bin -xzf nvim-linux64.tar.gz
+#tar -C ~/.local/bin -xzf nvim-linux64.tar.gz
